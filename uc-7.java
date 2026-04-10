61a393c1351e681362c16e6730ba5ad61bf85240
Raunak gautam <raunak23gautam@gmail.com>
2026-04-10 13:13:31 +0530

    Create uc-7.java

---
+++ uc-7.java
@@ -0,0 +1,11 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String result = "Hello " + String.join(", ", args);
+            System.out.println(result);
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
